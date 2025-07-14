050e7174cd0b09c192c5c295518050f38117d3c8
Brien-Austin <brienaustinclayton@gmail.com>
2025-07-14 13:57:22 +0530

    fix : cors policy

--- server/index.js
+++ server/index.js
@@ -9,7 +9,13 @@ const { authenticate } = require("./middleware/auth.middleware.js");
 const app = express();
 const PORT = process.env.PORT || 3005;
 dotenv.config();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use("/api/auth", authRouter);
 
